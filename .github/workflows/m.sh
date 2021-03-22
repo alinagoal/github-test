4db3ceaf98f18b16cf77a7fabb902d1bfe850b3b
alinagoal <63189288+alinagoal@users.noreply.github.com>
2021-03-22 14:13:53 +0545

    Update m.sh

--- .github/workflows/m.sh
+++ .github/workflows/m.sh
@@ -3,11 +3,11 @@
 set -ex
  
 PROJECT_ID='332'
-API_KEY='U2X2wQohZbacmobZzE4UT1lyY7ro7Lvx9mbigidA'
-CLIENT_ID='016abab353919cc3d940f7880f2ce777'
+API_KEY='NEfy258Tey5UNiQYsJ8HX3YoNrXLc4SG5DxwBdPZ'
+CLIENT_ID='f09ba6f55935528f92a149ae215ecfea'
 SCOPES=['"ViewTestResults"','"ViewAutomationHistory"']
 API_URL='https://7iggpnqgq9.execute-api.us-east-2.amazonaws.com/udbodh/api'
-INTEGRATION_JWT_TOKEN='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0X2lkIjozMzIsImFwaV9rZXlfaWQiOjI1MDksIm5hbWUiOiIiLCJkZXNjcmlwdGlvbiI6IiIsImljb24iOiIiLCJpbnRlZ3JhdGlvbl9uYW1lIjoiR2l0aHViIiwib3B0aW9ucyI6e30sImlhdCI6MTYxNjM4MzQ5MX0.6Q-l3XLwC3rodjm5AWTGTlHn4pc-wqYxMqRAH785n9E'
+INTEGRATION_JWT_TOKEN='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0X2lkIjozMzIsImFwaV9rZXlfaWQiOjI1MTMsIm5hbWUiOiIiLCJkZXNjcmlwdGlvbiI6IiIsImljb24iOiIiLCJpbnRlZ3JhdGlvbl9uYW1lIjoiVGVhbWNpdHkiLCJvcHRpb25zIjp7fSwiaWF0IjoxNjE2NDAxNjU2fQ.ISV53yEVmBRtEGHUbS7oLshiTRxoP_VJsWvPKdT2p_Q'
 INTEGRATIONS_API_URL='http://a64891dffd6b.ngrok.io'
  
 sudo apt-get update -y
