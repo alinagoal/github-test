2dcbe02e7dec0e59ecb37b4753b2bd1fa9062cb4
alinagoal <63189288+alinagoal@users.noreply.github.com>
2021-03-22 09:42:09 +0545

    Update m.sh

--- .github/workflows/m.sh
+++ .github/workflows/m.sh
@@ -7,17 +7,16 @@ API_KEY='U2X2wQohZbacmobZzE4UT1lyY7ro7Lvx9mbigidA'
 CLIENT_ID='016abab353919cc3d940f7880f2ce777'
 SCOPES=['"ViewTestResults"','"ViewAutomationHistory"']
 API_URL='https://7iggpnqgq9.execute-api.us-east-2.amazonaws.com/udbodh/api'
-INTEGRATION_JWT_TOKEN='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0X2lkIjozMzIsImFwaV9rZXlfaWQiOjI0ODAsIm5hbWUiOiIiLCJkZXNjcmlwdGlvbiI6IiIsImljb24iOiIiLCJpbnRlZ3JhdGlvbl9uYW1lIjoiR2l0aHViIiwib3B0aW9ucyI6e30sImlhdCI6MTYxNjE0NDgxNn0.6dUIS6GpJBhTfdIJz4Fxmq68FIbUDxfuFq3-xmNK0bQ'
-INTEGRATIONS_API_URL='http://f1fadec276a6.ngrok.io'
+INTEGRATION_JWT_TOKEN='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0X2lkIjozMzIsImFwaV9rZXlfaWQiOjI1MDksIm5hbWUiOiIiLCJkZXNjcmlwdGlvbiI6IiIsImljb24iOiIiLCJpbnRlZ3JhdGlvbl9uYW1lIjoiR2l0aHViIiwib3B0aW9ucyI6e30sImlhdCI6MTYxNjM4MzQ5MX0.6Q-l3XLwC3rodjm5AWTGTlHn4pc-wqYxMqRAH785n9E'
+INTEGRATIONS_API_URL='http://eb881f01da09.ngrok.io'
  
-sudo apt-get update -y
-sudo apt-get install -y jq
+apt-get update -y
+apt-get install -y jq
  
 #Trigger test run
 TEST_RUN_ID="$( \
   curl -X POST -G ${INTEGRATIONS_API_URL}/api/integrations/github/${PROJECT_ID}/events \
     -d 'token='$INTEGRATION_JWT_TOKEN''\
-    -d 'projectId='$PROJECT_ID''\
     -d 'triggerType=Deploy'\
   | jq -r '.test_run_id')"
  
@@ -49,4 +48,3 @@ TEST_RUN_RESULT="$( \
   | jq -r '.[0].status' \
 )"
 echo "Qualiti E2E Tests ${TEST_RUN_RESULT}"
-
