9ef0d5ce37c7b0a73cb06b2185f149a245cd6923
alinagoal <63189288+alinagoal@users.noreply.github.com>
2021-03-22 13:43:52 +0545

    Update m.sh

--- .github/workflows/m.sh
+++ .github/workflows/m.sh
@@ -17,6 +17,7 @@ sudo apt-get install -y jq
 TEST_RUN_ID="$( \
   curl -X POST -G ${INTEGRATIONS_API_URL}/api/integrations/github/${PROJECT_ID}/events \
     -d 'token='$INTEGRATION_JWT_TOKEN''\
+    -d 'projectId='$PROJECT_ID''\
     -d 'triggerType=Deploy'\
   | jq -r '.test_run_id')"
  
