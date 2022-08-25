70b5abdc8d336c3a995fd4a912ae6c0f4d95d429
Alina Dangol <63189288+alinagoal@users.noreply.github.com>
2022-08-25 12:14:08 +0545

    Update m.sh

--- .github/workflows/m.sh
+++ .github/workflows/m.sh
@@ -2,13 +2,13 @@
 
   set -ex
 
-  API_KEY='<replace-with-api-key>'
-  INTEGRATIONS_API_URL='https://3000-qualitiai-qualitiapi-m2ve15vx7ti.ws-us62.gitpod.io'
+  API_KEY='14fbbf402317abc1'
+  INTEGRATIONS_API_URL='https://3000-qualitiai-qualitiapi-hvwg6w2mctw.ws-us63.gitpod.io'
   PROJECT_ID='3'
-  CLIENT_ID='4e8d6ce61e03c393cb968cb09ac84cfc'
+  CLIENT_ID='a5cdd625fbd159e02ecbd8021af509d8'
   SCOPES=['"ViewTestResults"','"ViewAutomationHistory"']
-  API_URL='https://3000-qualitiai-qualitiapi-m2ve15vx7ti.ws-us62.gitpod.io/public/api'
-  INTEGRATION_JWT_TOKEN='665b89c53f229f9c410dee5b870069c04107015afd6d3a7620417fde94df136b0fa380f7ac9c6584f770ec35ab99c8d9a0ec6f70e46c52ae03b74077e52c8cbc6c72e6b6524105889e6cfcfd3165c43d75f6dbbb6b72e529d845a2e057176956a940b9c2e84d2bef2e2348e2c6c078ed5bda2ff904fea9b86cf72aa98841e4351ecb8ebfccbe1e309c7560634d8fb502a2a8beb7965f75bec04fd0d2878aad411808bee4c502c63787accf586120728126e8d682b8495f7467cd4e178eeffc9bfbef9e44355ac1a5eb174ec3dfecc152b76c82d19d842fc7068881f411a7cb0383cdb20ff8c9483f9f71c1c1053cddce0353e25cb68437793346fd31437a7fa5d95f3ee94d737c27d722eb5650d8faa2|57b755dd98672df46ac0ea4ae28a82f6|b3664244a450ddfc26588e3b5597e519'
+  API_URL='https://3000-qualitiai-qualitiapi-hvwg6w2mctw.ws-us63.gitpod.io/public/api'
+  INTEGRATION_JWT_TOKEN='e86d38dc98e9c9eba59700a50f310bfa8d67b68b2246089d88a9a48d4a2db9e36b1bc0f665cb9af4a5e9f838261eae8f9aa8c986ef35a796d4401aee77547d5f35c975ba14d30991626129ea9e3b2a646022b5d5b781ac15807fce1d0370774605544fd4507c9417d8386678da48404901bbf49aba4db9d6c3aa976f08e4ea7adfc8c46143cc78be76ee8ecad81e7c3c60936a892cf8a0c020e62d7447d3ce113ae34b9b7516b2cdf08a8f6a69e840a4824a04b4e4a9333a3a492a5f908c01d439c3ea61678064be7fc5fb161d9b518fca9759be8e718dc30ce6fef0cff9bf756bbb1b368bf0e726b86fde6dbe4bd4a7b38f13f8a994372d59809f8d15d8f6ec21b234576c7668bf4033ede238613f69|161e3dc9563a4ca779deffc618b4ded6|b2da3727a14e97469b1e8d520ea7dd3b'
 
   sudo apt-get update -y
   sudo apt-get install -y jq
@@ -19,7 +19,6 @@
       -d 'token='$INTEGRATION_JWT_TOKEN''\
       -d 'triggeredBy=Deploy'\
       -d 'triggerType=automatic'\
-      -d 'e2e=false'\
     | jq -r '.test_run_id')"
 
   AUTHORIZATION_TOKEN="$( \
