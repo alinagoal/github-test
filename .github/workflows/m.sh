ee20314fbc9859e2d34ff5b2abf1e4a845b72225
Alina Dangol <63189288+alinagoal@users.noreply.github.com>
2022-08-29 13:40:04 +0545

    Update m.sh

--- .github/workflows/m.sh
+++ .github/workflows/m.sh
@@ -25,7 +25,7 @@
     curl -X POST -G ${API_URL}/token \
     -H 'client-id: '${CLIENT_ID}'' \
     -H 'x-api-key: '${API_KEY}'' \
-    | jq -r '.type')"
+    | jq -r '.type .token')"
   echo "AUTHORIZATION_TOKEN= ${AUTHORIZATION_TOKEN}"
 
   # Wait until the test run has finished
