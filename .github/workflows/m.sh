710dff2ee92c8d57f35b10d4b9756afa23473fea
Alina Dangol <63189288+alinagoal@users.noreply.github.com>
2022-09-23 12:39:08 +0545

    Update m.sh

--- .github/workflows/m.sh
+++ .github/workflows/m.sh
@@ -42,7 +42,6 @@
     STATUS="$( \
       curl -X GET ${INTEGRATIONS_API_URL}/integrations/github/${PROJECT_ID}/test-run-status?test_run_id=${TEST_RUN_ID} \
         -d 'token='$INTEGRATION_JWT_TOKEN''\
-        -H 'Authorization: Bearer '$AUTHORIZATION_TOKEN'' \
         | jq -r '.status' \
     )"
 
