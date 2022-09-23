9460b553f13b1d60e95907596f7718c00bd1a3b3
Alina Dangol <63189288+alinagoal@users.noreply.github.com>
2022-09-23 15:39:18 +0545

    Update m.sh

--- .github/workflows/m.sh
+++ .github/workflows/m.sh
@@ -34,9 +34,9 @@
     echo "We are on iteration ${I}"
 
     STATUS="$( \
-      curl -X GET ${INTEGRATIONS_API_URL}/integrations/github/${PROJECT_ID}/test-run-status?token=${INTEGRATION_JWT_TOKEN}\&testRunId=${TEST_RUN_ID} \
-      | jq -r '.status')"
-      
+      curl -X GET ${INTEGRATIONS_API_URL}/integrations/github/${PROJECT_ID}/test-run-status?token=${INTEGRATION_JWT_TOKEN}\&test_run_id=${TEST_RUN_ID} \
+        | jq -r '.status' \
+    )"
 
     ((I=I+1))
 
@@ -48,4 +48,3 @@
     exit 0;
   fi
   exit 1;
-  
