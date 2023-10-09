1499af1ae21751356135d405032d63cebc26b365
Alina Dangol <63189288+alinagoal@users.noreply.github.com>
2023-10-09 09:21:24 +0545

    Update m.sh

--- .github/workflows/m.sh
+++ .github/workflows/m.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -ex
 
-API_KEY='527b43ec14d48d6e'
+API_KEY='30e6a6b3d46222da'
 API_KEYS_URL='public/api-keys'
-CLIENT_ID='6bc56441d7a39130842cfbf55d30b9ac'
-BASE_API_URL='https://3000-qualitiai-qualitiapi-bregtn5ubga.ws-us105.gitpod.io'
+CLIENT_ID='b3e967461ee94519229727fd0450da06'
+BASE_API_URL='https://api.qualiti-dev.com'
 PROJECT_ID='1'
-API_JWT_TOKEN='217c7df10b84d23ab6b70a91d126fe968d8566bc11b04e95abfa784ae1c17f50cb3113ad7c72b8306c772dac8b31cd8ef75f0bbd97ea529710e3808a531d893b7bf52ce9a1ca372823b33e24fc37b761d7d31d901a43dd84e9994f689e34a4a69aadf67e63502300b63a70ae2c26c660e47c13bfd5d1f092fe50fe22b55b2e37ac4c207e2eef3d739697a36c9179539033c600df962c98a7bd8b425f6e23eea01ff8ba224bf5d1673e100bfe50cd3132b3018d79e3070434c5d2add9d21ad480dc59ff215ce92d11359272aa13d7a9e3f98c7385ade08b113241fb34dd0c62b6547e30c4db0cf66c527b05df59afe1071fe242c3d0485561e9c1c1972034e0ecfbf78b6a281ce5de8a1e684d4e268ab4|da3a763b632add03c74e8e3cc87332ee|92a10e08c867c36f81780377dda2b0db'
+API_JWT_TOKEN='85cc2ec85907bb8642ea07ab3c01031f6e7285a9ce7f1b993bb1956dfdf85178cebccc2373f666c39f7bd79214c2fe0b8351e2a15b1f6ed7a81987e3edc04b6d7ba55100c425d7fe2793adc2c660cbf54d9f9d704d587c0e1266d6eee49e31d8946ac1c7aeb42d920cf74d90c59d19b73b6d6313fae4a41b750d0c55c1e456e3cf9477f8a390574570ffc8a789c46f2ffe357a2d0a38fdc268feafa8824202f270980da4b77081808f667ad152da325f27e88c58e9f59f4b55807bf36d556e1f5c96c4701613535610152afcad7b006b37b497bcdb2b61a201f23c8d64c3bf4f3ed04454ff11fc9ada0892fb9cc8f9eff0ff571342efa48d349c2c83b02a55d2fe1a3e575192009e5dd4e9f8a1974cac|3e9dd7893476b4f6d213fcb92c902445|0c0182b6dcbddc8f54fb01d4323d1ddd'
 
 if hash apt-get 2>/dev/null; then
   if [ "$(id -u)" -ne 0 ] && hash sudo 2>/dev/null; then
@@ -25,7 +25,7 @@ curl -X POST -G "$BASE_API_URL/integrations/github/$PROJECT_ID/trigger-test-run"
   -d 'triggerType=Deploy'\
 | jq -r '.test_run_ids[0]')"
 
-if [ "$TEST_RUN_ID" == null -o -z "$TEST_RUN_ID" ]; then
+if [ "$TEST_RUN_ID" = null ] || [ -z "$TEST_RUN_ID" ]; then
   echo 'No test trigger found.'
   exit 0
 fi
